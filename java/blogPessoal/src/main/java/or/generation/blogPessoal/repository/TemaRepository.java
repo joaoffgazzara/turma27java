36f0b306ced3afa4786f5a2d9c1c5a5bba04567a
joaoffgazzara <joaoffgazzara@gmail.com>
2021-07-28 15:00:49 -0300

    BlogPessoalDia3

---
+++ java/blogPessoal/src/main/java/or/generation/blogPessoal/repository/TemaRepository.java
@@ -0,0 +1,11 @@
+package or.generation.blogPessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import or.generation.blogPessoal.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+}
