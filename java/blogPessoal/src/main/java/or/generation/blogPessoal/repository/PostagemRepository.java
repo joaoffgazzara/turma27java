f3d3d831728fee4f15c0e826ce459208f68e433c
joaoffgazzara <joaoffgazzara@gmail.com>
2021-07-28 14:04:27 -0300

    BlogPessoalDia2

---
+++ java/blogPessoal/src/main/java/or/generation/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,22 @@
+package or.generation.blogPessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import or.generation.blogPessoal.model.Postagem;
+
+@Repository	
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+	
+
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+
+	
+	
+	
+	
+	
+}
